6e699aa08a5e7d2044de0606d58d88d61dfdc651
Ace Gabriel P. Pasiliao <99068659+sarapmoace@users.noreply.github.com>
2023-10-18 14:34:27 +0800

    edit links

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,6 +10,6 @@ export class ApiService {
   constructor(private _http:HttpClient) { }
 
   getData(inputbarcode: any): Observable<any>{
-    return this._http.get('http://localhost:8001/api/products/lists/search/' + inputbarcode);
+    return this._http.get('http://172.16.100.40:8001/api/products/lists/search/' + inputbarcode);
   }
 }
